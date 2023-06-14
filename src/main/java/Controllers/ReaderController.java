6960cf01c6d10af23e54e33adee8f3268ee9f3d3
JarrodHoughton <136487417+JarrodHoughton@users.noreply.github.com>
2023-06-14 10:35:51 +0200

    Updated Reader Controller
    Added a userExists() endpoint for the Reader Service Layer in the RIPClient.

--- src/main/java/Controllers/ReaderController.java
+++ src/main/java/Controllers/ReaderController.java
@@ -4,10 +4,28 @@
  */
 package Controllers;
 
+import ServiceLayers.ReaderService_Impl;
+import ServiceLayers.ReaderService_Interface;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.Response;
+
 /**
  *
  * @author jarro
  */
+@Path("/readers")
 public class ReaderController {
+    private ReaderService_Interface readerService;
+
+    public ReaderController() {
+        this.readerService = new ReaderService_Impl();
+    }
     
+    @Path("/userExists/{email}")
+    @GET
+    public Response userExists(@PathParam("email") String email) {
+        return Response.ok().entity(String.valueOf(readerService.userExists(email))).build();
+    }
 }
