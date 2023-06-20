18c5c6e42a030ba37beeebfe85f02ec49fb5f553
JarrodHoughton <136487417+JarrodHoughton@users.noreply.github.com>
2023-06-20 14:42:06 +0200

    Updated Server For Approve Writer

--- src/main/java/Controllers/WriterController.java
+++ src/main/java/Controllers/WriterController.java
@@ -4,10 +4,28 @@
  */
 package Controllers;
 
+import ServiceLayers.WriterService_Impl;
+import ServiceLayers.WriterService_Interface;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.Response;
+
 /**
  *
  * @author jarro
  */
+@Path("/writer")
 public class WriterController {
+    private WriterService_Interface writerService;
+
+    public WriterController() {
+        this.writerService = new WriterService_Impl();
+    }
     
+    @Path("/addWriter/{readerId}")
+    @GET
+    public Response addWriter(@PathParam("readerId") Integer readerId) {
+        return Response.ok().entity(writerService.addWriter(readerId)).build();
+    }
 }
