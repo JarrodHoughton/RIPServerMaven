5cc3ef2cd270217bfca3fbdbac5d20bb461f73b2
KylynnvdM <vandermerwekylynn@gmail.com>
2023-07-05 13:38:42 +0200

    SMSController and SMSService layer

---
+++ src/main/java/ServiceLayers/SMSService_Impl.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ServiceLayers;
+
+/**
+ *
+ * @author Kylynn van der Merwe
+ */
+public class SMSService_Impl implements SMSService_Interface{
+
+    @Override
+    public String sendSMS(String phoneNumber) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
